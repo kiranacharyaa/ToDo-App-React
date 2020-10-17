f1e762270d3ebb8ba51378f1b4debbcdc86e113f
KiranAcharya <kiranacharya287@gmail.com>
2020-10-17 15:42:56 +0530

    Updates

---
+++ precache-manifest.761bc71855b4254a77a608591945ccc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b54fe4114b23249982c63ff56c3ed2a",
+    "url": "/ToDo-App-React/index.html"
+  },
+  {
+    "revision": "c12f148b1bf9ce308d24",
+    "url": "/ToDo-App-React/static/css/main.36dd2e3f.chunk.css"
+  },
+  {
+    "revision": "5e8895557e3d6a8e428f",
+    "url": "/ToDo-App-React/static/js/2.cbe6e83c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToDo-App-React/static/js/2.cbe6e83c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12f148b1bf9ce308d24",
+    "url": "/ToDo-App-React/static/js/main.de3293ce.chunk.js"
+  },
+  {
+    "revision": "40b50f999fac3c5d14d8",
+    "url": "/ToDo-App-React/static/js/runtime-main.feb2c63e.js"
+  }
+]);
